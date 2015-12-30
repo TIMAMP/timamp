b4d8acc80f8e3249b0ce2020d749f708422ab945
Wouter Van den Broeck <wouter@addith.be>
2015-12-30 22:41:52 +0100

    v2/preprocess now loads unaggregated source data.

--- v2/preprocess/us15a/README.md
+++ v2/preprocess/us15a/README.md
@@ -1,5 +1,5 @@
 
-<h1>process_csv_us15a</h1>
+<h1>process_csv_us15a_bis</h1>
 
 **Author: Wouter Van den Broeck**
 
@@ -7,9 +7,9 @@
 
 # Introduction
 
-This project hosts a NodeJS application that generates the `data.json` file loaded by the `us15a` case study in the TIMAMP app, i.e. `tipaths_03d`. The source data is loaded from `data.csv` and the required metadata is loaded from `metadata.json`. 
+This project hosts a NodeJS application that generates the `data.json` file loaded by the `us15a` case study in the TIMAMP app. The source data is loaded from `data.csv` and the needed metadata is loaded from `metadata.json`. 
 
-The run this application, execute:
+The run this application, run:
 
 ```
 node index.js
@@ -17,48 +17,23 @@ node index.js
 
 # Data
 
-- For scans in which we have identified rain, we have set the ___velocity___ to 0 so that no lines/particles are created there. This is a temporary fix until the viz setups can directly handle weather.
+Dataset `data.csv` provided (as `conditional_data_more_bands_out.csv`) by Garrett Bernstein on Dec. 10, 2015.
 
+Notes:
 
-### Query template
-
-The query is a variation of the query proposed [here](https://github.com/enram/case-study/tree/master/data/bird-migration-altitude-profiles#aggregation).
-This variation aggregate altitudes depending on the template parameter ___strataSize___.
-
-The resulting records also contain the values:
-TODO
+- It's the same format as `process_csv_us15a` except each row is a single scan, so there's no averaging, because we only have one scan per time interval (6 minutes), station, and altitude combo.
 
 ### Strata (Altitude Bands)
 
-| id in source csv | range | index in data.json |
-|:----------------:|-------|:--------------------:|
-| 1 | 0.2 - 1.6 km | 0 |
-| 2 | 1.6 - 3.0 km | 1 |
+In the source data the altitude bands are expressed as decimal values, e.g. 2.65, which represents the middle altitude in km of 100m altitude bands. The global altitude range is 0.2 - 3 km.
 
-### vertical_integrated_density
+## Node dependencies
 
-The following SQL was used to generate the data.
-
-```
-round(avg(bird_density),5) AS avg_bird_density,
-CASE
-        WHEN altitude_band = 1 THEN round((avg(bird_density) * 7 /5),5)
-        WHEN altitude_band = 2 THEN round((avg(bird_density) * 7 /5),5)
-END AS vertical_integrated_density
-GROUP BY
-    radar_id,
-    interval_start_time,
-    altitude_band
-```
-The `vertical_integrated_density` is thus the number of birds per km^2 per strata, obtained by multiplying the height of each strata with the `bird_density`, which is the number of birds per km^3. Both strata have a height of 1.4 km, thus the average density is multiplied by 1.4 (7/5 or 14/10).
-
-# Node dependencies
-
-### [node-csv](https://github.com/wdavidw/node-csv)
+#### [node-csv](https://github.com/wdavidw/node-csv)
 
 Used to read the records from the csv file.
 
-### [jsonfile](https://www.npmjs.com/package/jsonfile)
+#### [jsonfile](https://www.npmjs.com/package/jsonfile)
 
 Used to write records as json.
 
--- v2/preprocess/us15a/index.js
+++ v2/preprocess/us15a/index.js
@@ -10,8 +10,6 @@ var moment = require('moment');
 
 // cofiguration:
 var csv_path = "data.csv";
-var raw_data_path = "raw-data.json";
-var data_path = "data.json";
 var metadata_path = "metadata.json";
 //var id = "us15a";
 
@@ -19,239 +17,293 @@ var metadata_path = "metadata.json";
 var ri_radar_id = 0,
   ri_interval_start_time = 1,
   ri_altitude_band = 2,
-  ri_avg_u_speed = 3,
-  ri_avg_v_speed = 4,
+  ri_u_speed = 3,
+  ri_v_speed = 4,
   ri_avg_bird_density = 5,
   ri_vertical_integrated_density = 6,
-  ri_number_of_measurements = 7,
-  ri_speed = 8;
+  ri_speed = 7 /* enriched */;
 
-function start() {
-  fs.readFile(metadata_path, 'utf8', function (err, raw) {
+function loadMetadata(path, handler) {
+  console.log('# Loading metadata from ' + path);
+  fs.readFile(path, 'utf8', function (err, raw) {
     if (err) {
-      console.error("! Failed to read '" + metadata_path + "'. " + err);
+      console.error("! Failed to read '" + path + "'. " + err);
       throw err;
     }
 
     var metadata = JSON.parse(raw);
 
     // segment duration in milliseconds:
-    var intervalMs = metadata.segmentSize * 60 * 1000;
+    metadata.intervalMs = metadata.segmentSize * 60 * 1000;
 
-    var startMoment = moment.utc(metadata.dataFrom);
-    var endMoment = moment.utc(metadata.dataTill);
-    var startTime = startMoment.valueOf();
-    var endTime = endMoment.valueOf();
+    metadata.startMoment = moment.utc(metadata.dataFrom);
+    metadata.endMoment = moment.utc(metadata.dataTill);
+    metadata.startTime = metadata.startMoment.valueOf();
+    metadata.endTime = metadata.endMoment.valueOf();
 
     // the number of segments:
-    var segn = Math.floor((endTime - startTime) / intervalMs);
+    metadata.segmentCount = Math.floor((metadata.endTime - metadata.startTime) / metadata.intervalMs);
 
     // data structure to map radar ids to the radar index:
-    var radarIndices = {};
+    metadata.radarIndices = {};
     metadata.radars.forEach(function (radar, i) {
-      radarIndices[radar.id] = i;
+      metadata.radarIndices[radar.id] = i;
     });
 
-    // the number of strata:
-    var strn = metadata.strataCount;
+    handler(metadata);
+  });
+}
 
-    // the number of radars:
-    var radn = metadata.radars.length;
+function loadData(path, metadata, handler) {
+  console.log('# Loading data from ' + path);
+  var parseConfig = { auto_parse: true, auto_parse_date: true };
+  var parser = csv.parse(parseConfig, function (err, records) {
+    if (err) {
+      console.error("! Failed to read '" + csv_path + "'. " + err);
+      throw err;
+    }
 
-    // read the csv-data:
-    var parseConfig = { auto_parse: true, auto_parse_date: true };
-    var parser = csv.parse(parseConfig, function (err, records) {
-      if (err) {
-        console.error("! Failed to read '" + csv_path + "'. " + err);
-        throw err;
-      }
+    // remove the headers:
+    var headers = records.shift();
 
-      // remove the headers:
-      var headers = records.shift();
+    // Parse the dates and add speed based on u-speed and v-speed:
+    records.forEach(function (record) {
+      record[ri_interval_start_time] = new Date(record[ri_interval_start_time]);
 
-      // Parse the dates and normalize the altitude id:
-      records.forEach(function (record) {
-        record[ri_interval_start_time] = new Date(record[ri_interval_start_time]);
-        record[ri_altitude_band] = record[ri_altitude_band] - 1;
-      });
+      var uSpeed = record[ri_u_speed];
+      var vSpeed = record[ri_v_speed];
+      record.push(Math.sqrt(uSpeed * uSpeed + vSpeed * vSpeed));
+    });
 
-      // Sort the records by date:
-      records.sort(function (ra, rb) {
-        if (ra[ri_interval_start_time] < rb[ri_interval_start_time]) return -1;
-        if (ra[ri_interval_start_time] > rb[ri_interval_start_time]) return 1;
-        return 0;
-      });
+    // Sort the records by date:
+    records.sort(function (ra, rb) {
+      if (ra[ri_interval_start_time] < rb[ri_interval_start_time]) return -1;
+      if (ra[ri_interval_start_time] > rb[ri_interval_start_time]) return 1;
+      return 0;
+    });
 
-      // Check if the given dates are inside the min-max range given in
-      // the metadata:
-      var minDate = records[0][ri_interval_start_time];
-      var maxDate = records[records.length - 1][ri_interval_start_time];
-      if (minDate < startMoment.toDate()) {
-        throw new Error("minDate < startMoment, minDate: " + minDate
-          + ", startMoment: " + startMoment.toDate());
-      }
-      if (maxDate > endMoment.toDate()) {
-        throw new Error("maxDate > endMoment, maxDate: " + maxDate
-          + ", endMoment: " + endMoment.toDate());
-      }
+    // Check if the given dates are inside the min-max range given in
+    // the metadata:
+    var minDate = records[0][ri_interval_start_time];
+    var maxDate = records[records.length - 1][ri_interval_start_time];
+    if (minDate < metadata.startMoment.toDate()) {
+      throw new Error("minDate < startMoment, minDate: " + minDate
+        + ", startMoment: " + metadata.startMoment.toDate());
+    }
+    if (maxDate > metadata.endMoment.toDate()) {
+      throw new Error("maxDate > endMoment, maxDate: " + maxDate
+        + ", endMoment: " + metadata.endMoment.toDate());
+    }
 
-      var segi, stri, radi;
+    console.log('# Data loaded');
+    handler(records);
+  });
+  fs.createReadStream(path).pipe(parser);
+}
 
-      // Prepare the raw-data structure:
-      var rawData = [];
+/**
+ * Prepare the raw-data structure.
+ * @param metadata
+ * @param strataOption
+ */
+function initRawData(metadata, strataOption) {
+  var strn = strataOption.length;  // the number of strata
+  var radn = metadata.radars.length;  // the number of radars
+  var stri, radi;
+
+  var rawData = [];
+  for (radi = 0; radi < radn; radi++) {
+    var radData = []; // per radar
+    for (stri = 0; stri < strn; stri++) {
+      radData.push([]); // per strata
+    }
+    rawData.push(radData);
+  }
+
+  return rawData;
+}
+
+/**
+ * Prepare the TIMAMP data structure which is constructed such that
+ * it efficiently facilitates the interpolation operations needed when
+ * constructing the paths.
+ * @param metadata
+ * @param strataOption
+ */
+function initData(metadata, strataOption) {
+  var segn = metadata.segmentCount;
+  var strn = strataOption.length;  // the number of strata
+  var radn = metadata.radars.length;  // the number of radars
+  var segi, stri, radi;
+
+  var data = {
+    densities: [],
+    uSpeeds: [],
+    vSpeeds: [],
+    speeds: [],
+    avDensities: []
+  };
+  for (segi = 0; segi < segn; segi++) {
+    var densities = [];
+    var uSpeeds = [];
+    var vSpeeds = [];
+    var speeds = [];
+    for (stri = 0; stri < strn; stri++) {
+      var densities2 = [];
+      var uSpeeds2 = [];
+      var vSpeeds2 = [];
+      var speeds2 = [];
       for (radi = 0; radi < radn; radi++) {
-        var radData = []; // per radar
-        for (stri = 0; stri < strn; stri++) {
-          radData.push([]); // per strata
-        }
-        rawData.push(radData);
+        densities2.push([]);
+        uSpeeds2.push([]);
+        vSpeeds2.push([]);
+        speeds2.push([]);
+      }
+      densities.push(densities2);
+      uSpeeds.push(uSpeeds2);
+      vSpeeds.push(vSpeeds2);
+      speeds.push(speeds2);
+    }
+    data.densities.push(densities);
+    data.uSpeeds.push(uSpeeds);
+    data.vSpeeds.push(vSpeeds);
+    data.speeds.push(speeds);
+  }
+
+  return data;
+}
+
+function process(records, metadata, strataOption, handler) {
+  //console.log("strataOption:", strataOption);
+  var segn = metadata.segmentCount;
+  var strn = strataOption.length;  // the number of strata
+  var radn = metadata.radars.length;  // the number of radars
+  var segi, stri, radi;
+
+  var rawData = initRawData(metadata, strataOption);
+  var data = initData(metadata, strataOption);
+
+  // Parse the records:
+  records.forEach(function (record, ri) {
+
+    // delta time
+    var dt = record[ri_interval_start_time].getTime() - metadata.startTime;
+    if (dt < 0) { throw new Error("dt < 0"); }
+
+    segi = Math.floor(dt / metadata.intervalMs);
+    stri = -1;
+    var alti = record[ri_altitude_band] * 1000;
+    for (var i = 0; i < strataOption.length; i++) {
+      if (strataOption[i][0] <= alti && strataOption[i][1] > alti) {
+        stri = i;
+        break;
+      }
+    }
+    radi = metadata.radarIndices[record[ri_radar_id]];
+    //console.log(segi, segn, alti, stri, strn, radi, radn);
+
+    // add record in the raw data structure:
+    //try {
+    //  rawData[radi][stri].push(record);
+    //}
+    //catch(error) {
+    //  console.error(radi, radn, stri, strn);
+    //  console.error(rawData[radi]);
+    //  console.error(rawData[radi][stri]);
+    //  throw error;
+    //}
+
+    // add data in the TIMAMP data structure:
+    try {
+      data.densities[segi][stri][radi].push(record[ri_avg_bird_density]);
+      data.uSpeeds[segi][stri][radi].push(record[ri_u_speed]);
+      data.vSpeeds[segi][stri][radi].push(record[ri_v_speed]);
+      data.speeds[segi][stri][radi].push(record[ri_speed]);
+    }
+    catch(error) {
+      console.error(segi, segn, stri, strn, radi, radn);
+      console.error(data.densities[segi]);
+      console.error(data.densities[segi][stri]);
+      console.error(data.densities[segi][stri][radi]);
+      throw error;
+    }
+  });
+
+  // Detect segments without data for all radars:
+  //for (segi = 0; segi < segn; segi++) {
+  //  var segTime = new Date(startTime + segi * intervalMs);
+  //  for (stri = 0; stri < strn; stri++) {
+  //    var foundDensities = false;
+  //    for (radi = 0; radi < radn; radi++) {
+  //      if (data.densities[segi][stri][radi].length > 0) {
+  //        foundDensities = true;
+  //        break;
+  //      }
+  //      //data.uSpeeds[segi][stri][radi] = average(data.uSpeeds[segi][stri][radi]);
+  //      //data.vSpeeds[segi][stri][radi] = average(data.vSpeeds[segi][stri][radi]);
+  //      //data.speeds[segi][stri][radi] = average(data.speeds[segi][stri][radi]);
+  //    }
+  //    if (!foundDensities) {
+  //      console.log("- no densities in segment " + segi + " (" + segTime + ")" + ", strata " + stri);
+  //    }
+  //  }
+  //}
+
+  // Calculate the averages for each segment:
+  for (segi = 0; segi < segn; segi++) {
+    for (stri = 0; stri < strn; stri++) {
+      for (radi = 0; radi < radn; radi++) {
+        data.densities[segi][stri][radi] = average(data.densities[segi][stri][radi]);
+        data.uSpeeds[segi][stri][radi] = average(data.uSpeeds[segi][stri][radi]);
+        data.vSpeeds[segi][stri][radi] = average(data.vSpeeds[segi][stri][radi]);
+        data.speeds[segi][stri][radi] = average(data.speeds[segi][stri][radi]);
+      }
+    }
+  }
+
+  // The strata sizes in km:
+  data.strataSizes = strataOption.map(function (strata) {
+    return (strata[1] - strata[0]) / 1000;
+  });
+
+  handler(rawData, data);
+}
+
+function processData(records, metadata, strataIdx) {
+  if (strataIdx < metadata.strataOptions.length) {
+    process(records, metadata, metadata.strataOptions[strataIdx], function (rawData, data) {
+      var raw_data_path = "output/raw-data-" + strataIdx + ".json";
+      var data_path = "output/data-" + strataIdx + ".json";
+
+      if (!fs.existsSync("output")) {
+        fs.mkdir("output");
       }
 
-      // Prepare the TIMAMP data structure which is constructed such that
-      // it efficiently facilitates the interpolation operations needed when
-      // constructing the paths.
-      var data = {
-        densities: [],
-        uSpeeds: [],
-        vSpeeds: [],
-        speeds: [],
-        avDensities: []
-      };
-      for (segi = 0; segi < segn; segi++) {
-        var densities = [];
-        var uSpeeds = [];
-        var vSpeeds = [];
-        var speeds = [];
-        for (stri = 0; stri < strn; stri++) {
-          var densities2 = [];
-          var uSpeeds2 = [];
-          var vSpeeds2 = [];
-          var speeds2 = [];
-          for (radi = 0; radi < radn; radi++) {
-            densities2.push([]);
-            uSpeeds2.push([]);
-            vSpeeds2.push([]);
-            speeds2.push([]);
-          }
-          densities.push(densities2);
-          uSpeeds.push(uSpeeds2);
-          vSpeeds.push(vSpeeds2);
-          speeds.push(speeds2);
-        }
-        data.densities.push(densities);
-        data.uSpeeds.push(uSpeeds);
-        data.vSpeeds.push(vSpeeds);
-        data.speeds.push(speeds);
-      }
+      //jsonfile.spaces = 2;
 
-      // Parse the records and put all values for each segment in lists:
-      var uSpeed, vSpeed, speed;
-      records.forEach(function (record) {
-        var dt = record[ri_interval_start_time].getTime() - startTime;
-        if (dt < 0) { throw new Error("dt < 0"); }
-
-        segi = Math.floor(dt / intervalMs);
-        stri = record[ri_altitude_band];
-        radi = radarIndices[record[ri_radar_id]];
-        //console.log(segi, segn, stri, strn, radi, radn);
-
-        uSpeed = record[ri_avg_u_speed];
-        vSpeed = record[ri_avg_v_speed];
-        speed = Math.sqrt(uSpeed * uSpeed + vSpeed * vSpeed);
-        record.push(speed);
-
-        // add record in the raw-data structure:
-        rawData[radi][stri].push(record);
-
-        // add data in the TIMAMP data structure:
-        try {
-          data.densities[segi][stri][radi].push(record[ri_avg_bird_density]);
-          data.uSpeeds[segi][stri][radi].push(uSpeed);
-          data.vSpeeds[segi][stri][radi].push(vSpeed);
-          data.speeds[segi][stri][radi].push(speed);
-        }
-        catch(error) {
-          console.log(segi, segn, stri, strn, radi, radn);
-          console.log(data.densities[segi]);
-          console.log(data.densities[segi][stri]);
-          console.log(data.densities[segi][stri][radi]);
-          throw error;
-        }
-      });
-
-      // Detect segments without data for all radars:
-      //for (segi = 0; segi < segn; segi++) {
-      //  var segTime = new Date(startTime + segi * intervalMs);
-      //  for (stri = 0; stri < strn; stri++) {
-      //    var foundDensities = false;
-      //    for (radi = 0; radi < radn; radi++) {
-      //      if (data.densities[segi][stri][radi].length > 0) {
-      //        foundDensities = true;
-      //        break;
-      //      }
-      //      //data.uSpeeds[segi][stri][radi] = average(data.uSpeeds[segi][stri][radi]);
-      //      //data.vSpeeds[segi][stri][radi] = average(data.vSpeeds[segi][stri][radi]);
-      //      //data.speeds[segi][stri][radi] = average(data.speeds[segi][stri][radi]);
-      //    }
-      //    if (!foundDensities) {
-      //      console.log("- no densities in segment " + segi + " (" + segTime + ")" + ", strata " + stri);
-      //    }
+      //console.log("# Writing raw-data at " + raw_data_path);
+      //jsonfile.writeFile(raw_data_path, rawData, function (err) {
+      //  if (err) {
+      //    console.error("Failed to write '" + raw_data_path + "'. " + err);
+      //    throw err;
       //  }
-      //}
 
-      // Calculate the averages for each segment:
-      for (segi = 0; segi < segn; segi++) {
-        for (stri = 0; stri < strn; stri++) {
-          for (radi = 0; radi < radn; radi++) {
-            data.densities[segi][stri][radi] = average(data.densities[segi][stri][radi]);
-            data.uSpeeds[segi][stri][radi] = average(data.uSpeeds[segi][stri][radi]);
-            data.vSpeeds[segi][stri][radi] = average(data.vSpeeds[segi][stri][radi]);
-            data.speeds[segi][stri][radi] = average(data.speeds[segi][stri][radi]);
-          }
-        }
-      }
-
-      // The strata height in km:
-      var strataHeight = metadata.maxAltitude / metadata.strataCount / 1000;
-
-      // Calculate average densities per radar-altitude combination, integrated
-      // over the strata height. These numbers thus represent the number of birds
-      // per square km in a given strata.
-      //for (stri = 0; stri < strn; stri++) {
-      //  var avds = [];
-      //  for (radi = 0; radi < radn; radi++) {
-      //    var dsum = 0;
-      //    for (segi = 0; segi < segn; segi++) {
-      //      dsum += data.densities[segi][stri][radi];
-      //    }
-      //    avds[radi] = dsum / segn * strataHeight;
-      //  }
-      //  data.avDensities.push(avds);
-      //}
-
-      // write the raw data in a json format:
-      console.log("# Writing " + raw_data_path);
-      jsonfile.writeFile(raw_data_path, rawData, function (err) {
-        if (err) {
-          console.error("Failed to write '" + raw_data_path + "'. " + err);
-        }
-
-        console.log("# Writing " + data_path);
+        console.log("# Writing TIMAMP-data at " + data_path);
         jsonfile.writeFile(data_path, data, function (err) {
           if (err) {
             console.error("Failed to write '" + data_path + "'. " + err);
+            throw err;
           }
-          else {
-            console.error("# Done");
-          }
+
+          processData(records, metadata, strataIdx + 1);
         });
       });
 
-    });
-
-    console.log('# Reading ' + csv_path);
-    fs.createReadStream(csv_path).pipe(parser);
-  });
+    //});
+  }
+  else {
+    console.log("# Complete");
+  }
 }
 
 /**
@@ -270,4 +322,8 @@ function average(ary, undefAv) {
   return r / len;
 }
 
-start();
+loadMetadata(metadata_path, function (metadata) {
+  loadData(csv_path, metadata, function (records) {
+    processData(records, metadata, 0);
+  });
+});
