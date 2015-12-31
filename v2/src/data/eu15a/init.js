c57a45d469245d52b88edc7fe1bf26dbf556216a
Wouter Van den Broeck <wouter@addith.be>
2015-12-31 11:10:41 +0100

    TIMAMP v2: The EU case is now also loaded from Json files.

--- v2/src/data/eu15a/init.js
+++ v2/src/data/eu15a/init.js
@@ -2,13 +2,12 @@
  * Created by wouter on 22/09/2015.
  */
 
-
 /**
  * eu15a case study constructor.
  */
 var eu15a = function () {
 
-  var caseStudy = enram.caseStudy("eu15a", DBDataService());
+  var caseStudy = enram.caseStudy("eu15a", JsonDataService());
 
   caseStudy.getProjection = function (caseStudy, mapWidth, mapHeight) {
     return d3.geo.mercator()
